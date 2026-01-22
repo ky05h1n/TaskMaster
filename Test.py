144197d788d6bf5a62cd03bb3da04e19faebd6b2
ky05h1n <mrmy0wnbus1n3ss@hotmail.com>
2026-01-22 19:50:26 +0100

    Add colored output and fix class methods

--- Test.py
+++ Test.py
@@ -1,51 +1,63 @@
 import subprocess
 import yaml
-import time 
+import time
+import readline
+import threading
 
 
-def read_conf():
+GREEN = "\033[92m"
+RED = "\033[91m"
+YELLOW = "\033[93m"
+CYAN = "\033[96m"
+RESET = "\033[0m"
+
+
+class TaskMaster:
     
-    with open("conf.yaml", 'r') as file :
-        config = yaml.safe_load(file)
-    return config['programs']
+    def __init__(self, configfile):
+        self.configfile = configfile
+        self.programs = {}
+        self.pid_list = {}
 
-
-def run_progrms(obj):
+    def run(self, programs=None):
+        if programs is None:
+            programs = self.programs
+        for prog , item in programs.items():
+            cmd = item.get('cmd')
+            proc = subprocess.Popen(cmd.split())
+            self.pid_list[proc.pid] = (proc, prog, item)
+            print(f"{CYAN}▶ Started program: {prog} with PID: {proc.pid}{RESET}")
+            time.sleep(1)
+        return self.pid_list
     
-    pid_list = {}
-    for prog , item in obj.items():
-        cmd = item.get('cmd')
-        proc = subprocess.Popen(cmd.split())
-        pid_list[proc.pid] = (proc, prog, item)
-        print(f"Started program: {prog} with PID: {proc.pid}")
-    return pid_list
-
-
-def check_auto0start(item):
-    autostart = item.get('autostart', False)
-    return autostart
-
-def chek_status(pid_list, obj):
+    def load_config(self):
+        with open(self.configfile, 'r') as file:
+            self.programs = yaml.safe_load(file)
+        return self.programs['programs']
     
-    while pid_list:
-        for pid in list(pid_list.keys()):
-            proc, prog, item = pid_list[pid]
-            autorestart = item.get('autorestart')
-            if proc.poll() is None:
-                print(f"Program {prog} with PID: {proc.pid} is still running.")
-            else:
-                print(f"Program {prog} with PID: {proc.pid} has terminated.")
-                pid_list.pop(pid)
-            if autorestart == True:
-                print(f"Restarting program: {prog}")            
+    def monitor(self):
+        
+            for pid in list(self.pid_list.keys()):
+                proc, prog, item = self.pid_list[pid]
+                autorestart = item.get('autorestart')
+                if proc.poll() is None:
+                    print(f"{GREEN}● Program {prog} with PID: {proc.pid} is still running.{RESET}")
+                    time.sleep(1)
+                else:
+                    print(f"{RED}✖ Program {prog} with PID: {proc.pid} has terminated.{RESET}")
+                    self.pid_list.pop(pid)
+                    time.sleep(1)
+                    if autorestart == True:
+                        print(f"{YELLOW}↻ Restarting program: {prog}{RESET}")
+                        time.sleep(1)
+                        self.pid_list.update(self.run({prog: item}))
                 
-        time.sleep(1)
-
+                
 if __name__ == "__main__":
     
     
-    obj = read_conf()
-    pid_list = run_progrms(obj)
-    chek_status(pid_list, obj)
-    
-    
+    Obj = TaskMaster("conf.yaml")
+    Obj.programs = Obj.load_config()
+    Obj.pid_list.update(Obj.run())
+    while True:
+        Obj.monitor()
